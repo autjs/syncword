ee47c5904e804aa04902db4912811ddb072898b3
worksite <espace@aliyun.com>
2020-07-10 13:58:28 +0800

    koa ejs

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const app = require('./koa')
 const conf = require('./config')
 
+
+// 当我们配制 ejs 模板引擎的时候
+// 配制的代码一定要放在router 配制代码之前 
+// 这个比较容易被忽略
+const views = require('koa-views')
+
+app.use(views('views',{
+  map: {
+    html: 'ejs'
+  }
+}))
+
+
+
 const router = require('./router')
+
 
 app.use(ctx => {
   ctx.body = 'hello Syncword'
--- router/routes/user.js
+++ router/routes/user.js
@@ -3,11 +3,24 @@ const router = new KoaRouter()
 
 
 // login
+
+router.get('/login', async ctx => {
+  await ctx.render('login')
+})
+
 router.post('/login', async ctx => {
   ctx.body = `login api`
 })
 
 // register
+
+router.get('/register', async ctx => {
+  // 把 register.html 渲染出来..
+  await ctx.render('register', {
+    name: '注册页面'
+  })
+})
+
 router.post('/register', async ctx => {
   ctx.body = `register api`
 })
